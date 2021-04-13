602a0007fbd991d4f08b20b463fafe245193c5d3
Ekwuno Obinna <obinnacodes@gmail.com>
2021-04-13 16:02:53 +0400

    lets go 🚀

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,26 +1,29 @@
+require("dotenv").config();
 module.exports = {
-  siteMetadata: {
-    title: "groundHogDay",
-  },
-  plugins: [
-    {
-      resolve: "gatsby-source-shopify",
-      options: {
-        shopName: "",
-        accessToken: "",
-      },
-    },
-    "gatsby-plugin-gatsby-cloud",
-    "gatsby-plugin-image",
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "images",
-        path: "./src/images/",
-      },
-      __key: "images",
-    },
-  ],
+	siteMetadata: {
+		title: "groundHogDay",
+	},
+
+	plugins: [
+		{
+			resolve: "gatsby-source-shopify-experimental",
+			options: {
+				apiKey: process.env.SHOPIFY_ADMIN_API_KEY,
+				password: process.env.SHOPIFY_ADMIN_PASSWORD,
+				storeUrl: process.env.GATSBY_SHOPIFY_STORE_URL,
+			},
+		},
+		"gatsby-plugin-gatsby-cloud",
+		"gatsby-plugin-image",
+		"gatsby-plugin-sharp",
+		"gatsby-transformer-sharp",
+		{
+			resolve: "gatsby-source-filesystem",
+			options: {
+				name: "images",
+				path: "./src/images/",
+			},
+			__key: "images",
+		},
+	],
 };
